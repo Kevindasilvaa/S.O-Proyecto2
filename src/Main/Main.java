6668586186d3a78b2714c6f7ba79a7a054d3d185
Kevin Da Silva <kdasilva@correo.unimet.edu.ve>
2024-02-16 21:17:58 -0400

    creacion clase Personaje

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,6 +4,8 @@
  */
 package Main;
 
+import Personaje.Personaje;
+
 /**
  *
  * @author Kevin
@@ -15,6 +17,27 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int cantidad_1 = 0;
+        int cantidad_2 = 0;
+        int cantidad_3 = 0;
+        int contador_id = 0;
+        for (int i = 0; i < 15; i++) {
+            contador_id += 1;
+            Personaje p = new Personaje(contador_id,"Disney");
+            if(p.getNivel() == 1){
+                cantidad_1 += 1;
+            }
+            if(p.getNivel() == 2){
+                cantidad_2 += 1;
+            }
+            if(p.getNivel() == 3){
+                cantidad_3 += 1;
+            }
+            //System.out.println(p.isHabilidades() +" " + p.isPuntosVida() + " " + p.isFuerza() + " " + p.isAgilidad() + " "+ p.getNivel()+"\n");
+        }
+        System.out.println("cantidad 1: " + cantidad_1);
+        System.out.println("cantidad 2: " + cantidad_2);
+        System.out.println("cantidad 3: " + cantidad_3);
     }
     
 }
